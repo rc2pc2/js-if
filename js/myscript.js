6454bc4aea2032b3358a574c8ceb272411dd2d78
rc2pc2 <petriccarcc@gmail.com>
2023-05-12 12:02:09 +0200

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,63 @@
+
+// // AND
+// console.log( (4 == 4) && (3 == 3) ); // true
+// console.log( (4 == 4) && (2 == 3) ); // false
+// console.log( (3 == 4) && (3 == 3) ); // false
+// console.log( (3 == 4) && (2 == 3) ); // false
+
+// // OR
+// console.log( (4 == 4) || (3 == 3) ); // true
+// console.log( (4 == 4) || (2 == 3) ); // true
+// console.log( (3 == 4) || (3 == 3) ); // true
+// console.log( (3 == 4) || (2 == 3) ); // false
+
+// // NOT
+// console.log( 4 == 4) // true
+// console.log( !(4 == 4)) // false
+// console.log( !false ) // true
+
+
+// let utenteLoggato = -124124561;
+
+// // # L'utente loggato è true?
+// if (utenteLoggato == true){  // ? se si entro qui dentro
+//     console.log('Utente loggato ha valore ' + utenteLoggato + ', ed è true');
+// } else { // § altrimenti entro qui dentro
+//     console.log('Utente loggato ha valore ' + utenteLoggato + ', ed è false');
+// }
+
+//?  (utenteLoggato == true) è uguale a ( utenteLoggato )
+//§  (utenteLoggato == false) è uguale a ( !utenteLoggato )
+
+/**
+ *
+ * Chiediamo all'utente due parole in successione.
+    Andiamo poi a verificare quale delle due parole è più lunga e stampiamo in console un messaggio appropriato.
+ *
+ */
+
+// ? - dichiariamo una variabile CONST randomWord e la popoliamo con un prompt
+// ? - dichiariamo una seconda variabile CONST randomOtherWord e la popoliamo con un prompt
+
+// ? -  if  (verifico quale delle due parole sia più lunga)
+    // § - se  la prima parola è la più lunga ...
+        // # - stampiamo in console un messaggio appropriato
+
+    // § - se invece la seconda parola è la più lunga ...
+        // # - stampiamo in console un messaggio appropriato
+
+    // § - altrimenti significa che la parole saranno lunghe ugualmente
+        // # - stampiamo in console un messaggio appropriato
+
+
+// const randomWord = prompt('Insert a random word');
+// const randomOtherWord = prompt('Insert a random word');
+
+// if (randomWord.length > randomOtherWord.length){
+//     console.log('La prima parola è la più lunga');
+// } else if ( randomWord.length < randomOtherWord.length) {
+//     console.log('La seconda parola è la più lunga');
+// } else {
+//     console.log('Le due parole hanno la medesima lunghezza');
+// }
+
